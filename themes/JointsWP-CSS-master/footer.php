9605a3b4d99fe267aac228156de9df6942b53243
Tu Luong <tu@delindesign.com>
2018-04-17 10:40:58 -0400

    Footer desktop

--- themes/JointsWP-CSS-master/footer.php
+++ themes/JointsWP-CSS-master/footer.php
@@ -1,37 +1,52 @@
 <?php
 /**
- * The template for displaying the footer. 
+ * The template for displaying the footer.
  *
  * Comtains closing divs for header.php.
  *
  * For more info: https://developer.wordpress.org/themes/basics/template-files/#template-partials
- */			
+ */
  ?>
-					
+
 				<footer class="footer" role="contentinfo">
-					
-					<div class="inner-footer grid-x grid-margin-x grid-padding-x">
-						
-						<div class="small-12 medium-12 large-12 cell">
-							<nav role="navigation">
-	    						<?php joints_footer_links(); ?>
-	    					</nav>
-	    				</div>
-						
-						<div class="small-12 medium-12 large-12 cell">
-							<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+
+					<div class="grid-container">
+
+						<div id="footer-sidebar" class="footer-links secondary grid-x">
+							<div id="footer-sidebar1" class="footer-menu medium-2 large-2 cell">
+								<?php dynamic_sidebar('footer-sidebar-1'); ?>
+							</div>
+							<div id="footer-sidebar2" class="footer-menu medium-2 large-2 cell">
+								<?php dynamic_sidebar('footer-sidebar-2'); ?>
+							</div>
+							<div id="footer-sidebar3" class="footer-menu medium-2 large-2 cell">
+								<?php dynamic_sidebar('footer-sidebar-3'); ?>
+							</div>
+							<div id="footer-sidebar4" class="footer-menu medium-2 large-2 cell">
+								<?php dynamic_sidebar('footer-sidebar-4'); ?>
+							</div>
+							<div id="footer-sidebar5" class="footer-menu medium-2 large-2 cell">
+								<?php dynamic_sidebar('footer-sidebar-5'); ?>
+							</div>
+							<div id="footer-sidebar6" class="footer-menu medium-2 large-2 cell">
+								<?php dynamic_sidebar('footer-sidebar-6'); ?>
+							</div>
 						</div>
-					
-					</div> <!-- end #inner-footer -->
-				
+
+						<div class="inner-footer grid-x">
+							<p class="source-org copyright">&copy; <?php echo date('Y'); ?> | <?php bloginfo('name'); ?>.</p>
+						</div> <!-- end #inner-footer -->
+
+					</div>
+
 				</footer> <!-- end .footer -->
-			
+
 			</div>  <!-- end .off-canvas-content -->
-					
+
 		</div> <!-- end .off-canvas-wrapper -->
-		
+
 		<?php wp_footer(); ?>
-		
+
 	</body>
-	
+
 </html> <!-- end page -->
--- themes/JointsWP-CSS-master/functions.php
+++ themes/JointsWP-CSS-master/functions.php
@@ -1,57 +1,57 @@
 <?php
-/** 
+/**
  * For more info: https://developer.wordpress.org/themes/basics/theme-functions/
  *
- */			
-	
+ */
+
 // Theme support options
-require_once(get_template_directory().'/functions/theme-support.php'); 
+require_once(get_template_directory().'/functions/theme-support.php');
 
 // WP Head and other cleanup functions
-require_once(get_template_directory().'/functions/cleanup.php'); 
+require_once(get_template_directory().'/functions/cleanup.php');
 
 // Register scripts and stylesheets
-require_once(get_template_directory().'/functions/enqueue-scripts.php'); 
+require_once(get_template_directory().'/functions/enqueue-scripts.php');
 
 // Register custom menus and menu walkers
-require_once(get_template_directory().'/functions/menu.php'); 
+require_once(get_template_directory().'/functions/menu.php');
 
 // Register sidebars/widget areas
-require_once(get_template_directory().'/functions/sidebar.php'); 
+require_once(get_template_directory().'/functions/sidebar.php');
 
 // Makes WordPress comments suck less
-require_once(get_template_directory().'/functions/comments.php'); 
+require_once(get_template_directory().'/functions/comments.php');
 
 // Replace 'older/newer' post links with numbered navigation
-require_once(get_template_directory().'/functions/page-navi.php'); 
+require_once(get_template_directory().'/functions/page-navi.php');
 
 // Adds support for multiple languages
-require_once(get_template_directory().'/functions/translation/translation.php'); 
+require_once(get_template_directory().'/functions/translation/translation.php');
 
 // Adds site styles to the WordPress editor
-// require_once(get_template_directory().'/functions/editor-styles.php'); 
+// require_once(get_template_directory().'/functions/editor-styles.php');
 
 // Remove Emoji Support
-// require_once(get_template_directory().'/functions/disable-emoji.php'); 
+// require_once(get_template_directory().'/functions/disable-emoji.php');
 
 // Related post function - no need to rely on plugins
-// require_once(get_template_directory().'/functions/related-posts.php'); 
+// require_once(get_template_directory().'/functions/related-posts.php');
 
 // Use this as a template for custom post types
 // require_once(get_template_directory().'/functions/custom-post-type.php');
 
 // Customize the WordPress login menu
-// require_once(get_template_directory().'/functions/login.php'); 
+// require_once(get_template_directory().'/functions/login.php');
 
 // Customize the WordPress admin
-// require_once(get_template_directory().'/functions/admin.php'); 
+// require_once(get_template_directory().'/functions/admin.php');
 
 // Add Lato from Google Fonts
 function wpb_add_google_fonts() {
- 
-wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700', false ); 
+
+wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700', false );
 }
- 
+
 add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
 
 // Add support for WooCommerce
@@ -59,7 +59,6 @@ function mytheme_add_woocommerce_support() {
 	add_theme_support( 'woocommerce', array(
 		'thumbnail_image_width' => 150,
 		'single_image_width'    => 300,
-
         'product_grid'          => array(
             'default_rows'    => 4,
             'min_rows'        => 2,
@@ -199,9 +198,9 @@ function my_mce4_options($init) {
         "0076a9", "MN Blue",
         "ffa400", "MN Saturated Yellow",
         "f9e0a4", "MN Pale Yellow",
-		"231f20", "MN Dark Gray",
-		"ffffff", "White",
-		"000000", "Black"
+				"231f20", "MN Dark Gray",
+				"ffffff", "White",
+				"000000", "Black"
     ';
 
     // build colour grid default+custom colors
@@ -213,3 +212,19 @@ function my_mce4_options($init) {
 
     return $init;
 }
+
+if ( function_exists('register_sidebar') ) {
+	register_sidebar();
+	register_sidebars(6,
+		array(
+			'name' 					=> 'Footer Sidebar %d',
+			'id' 						=> 'footer-sidebar-%d',
+			'description' 	=> 'Appears in the footer area',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget' 	=> '</aside>',
+			'before_title' 	=> '<h3 class="footer-title">',
+			'after_title' 	=> '</h3>',
+		)
+	);
+}
+
