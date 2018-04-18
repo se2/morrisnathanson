b6a9a64727f0db72e1a95e82d33ceb1e1bc9b7e7
Tu Luong <tu@delindesign.com>
2018-04-17 23:56:02 -0400

    Implement Stories desktop styling, update db, add Column Shortcode plugin

--- themes/JointsWP-CSS-master/templates/template-stories.php
+++ themes/JointsWP-CSS-master/templates/template-stories.php
@@ -18,7 +18,7 @@ get_header(); ?>
 	<div class="story-container">
 		<div class="grid-container">
 			<div class="grid-x">
-				<div class="medium-10 cell">
+				<div class="small-12 medium-10 cell story-left-content">
 					<?php
 						$story_query = new WP_Query(
 							array(
@@ -28,17 +28,43 @@ get_header(); ?>
 
 						if ($story_query->have_posts()) :
 							while ($story_query->have_posts()) : $story_query->the_post(); ?>
-
-							<div class="medium-12 cell">
+							<div class="grid-x story-item gray-line">
 								<!-- Title -->
-								<div>
-									<h4 class="story-title">
-										<?php the_title(); ?>
-										<span class='story-date'>
-											<?php the_field('story_year'); ?>
-										<span>
-									</h4>
+								<div class="small-12 medium-12 cell story-title-section">
+									<div class="post-title">
+										<h4 class="story-title">
+											<?php the_title(); ?>
+											<span class='story-date'>
+												<?php the_field('story_year'); ?>
+											<span>
+										</h4>
+									</div>
 								</div>
+								<!-- Info -->
+								<div class="small-12 medium-6 cell story-info-section">
+									<div class="bg-cover story-featured-img" style="background-image:url('<?php the_field('intro_vertical_image'); ?>');"></div>
+								</div>
+								<div class="medium-1 cell"></div>
+								<div class="small-12 medium-5 cell story-info-section">
+									<div class="story-map bg-contain" style="background-image:url('<?php the_field('story_map_image'); ?>');"></div>
+									<div class="story-quote">
+										<p>
+											<?php the_field('story_quote'); ?>
+											<br>
+											<span class="story-quote-author">- Morris</span>
+										</p>
+									</div>
+								</div>
+								<!-- Cover -->
+								<div class="small-12 medium-12 cell bg-cover story-cover-1" style="background-image:url('<?php the_field('horitonzal_image_1'); ?>');"></div>
+								<!-- Content -->
+								<div class="small-12 medium-12 cell story-content-section">
+									<div class="post-content">
+										<?php the_field('story_content'); ?>
+									</div>
+								</div>
+								<!-- Cover -->
+								<div class="small-12 medium-12 cell bg-cover story-cover-1" style="background-image:url('<?php the_field('horitonzal_image_2'); ?>');"></div>
 							</div>
 
 							<?php endwhile; ?>
